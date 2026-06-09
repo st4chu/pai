1cde28e35319a71002e0b90acffb43f9c488dbac
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-09 19:58:33 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- api/connection.php
+++ api/connection.php
@@ -1,29 +1,24 @@
 <?php
 
 class DB{
-    public $host = "ms-tc-sql.postgres.database.azure.com";
-    public $database = "azure-db";
-    public $username = "kalendarz";
-    public $password = "QWEasd123";
-    public $conn;
-    
+    $serverName - "tc-ms-us.database.windows.net"   ;
+    $connectionOptions = array(
+        "Database" => "kalendarz",
+        "Uid" => "kalendarz",
+        "PWD" => "QWEasd123"
+    );
 
     public function getConn(){
         $this->conn = null;
 
     try {
-        $conn = new PDO("sqlsrv:server = ms-sql-tc.postgres.database.azure.com; Database = events", "kalendarz", "QWEasd123");
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $e) {
         print("Error connecting to SQL Server.");
         die(print_r($e));
     }
-
-    $connectionInfo = array("UID" => "CloudSA349e26e7", "pwd" => "{your_password_here}", "Database" => "azure-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:tc-ms-db.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
-
         return $this->conn;
     }
 }
