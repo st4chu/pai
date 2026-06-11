9c4f24b5b366498dd13d7ba449b3f27fe50f344e
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-11 22:06:22 +0200

    pliki

--- api/connection.php
+++ api/connection.php
@@ -2,11 +2,11 @@
 
 class DB{
     public $conn = false;
-    public $login = file_get_contents("../keys/login.txt");
-    public $pass = file_get_contents("../keys/pass.txt");
 
     public function __construct(){
         try {
+            $login = file_get_contents("../keys/login.txt");
+            $pass = file_get_contents("../keys/pass.txt");
             $connectionInfo = array("UID" => $this->login, "pwd" => $this->pass, "Database" => "kalendarz", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
             $serverName = "tcp:kalendarz-sqldb.database.windows.net,1433";
             $this->conn = sqlsrv_connect($serverName, $connectionInfo);
