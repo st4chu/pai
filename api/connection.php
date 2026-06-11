b5070b57bb1e23ba0f3d12e74cbc2761cc5c201f
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-11 22:01:18 +0200

    debugging connection

--- api/connection.php
+++ api/connection.php
@@ -1,18 +1,21 @@
 <?php
 
 class DB{
+    public $conn;
+    public $login =file_get_contents("../keys/login.txt");
+    public $pass = file_get_contents("../keys/pass.txt");
+
     public function __construct(){
-    try {
-        $login = file_get_contents("../keys/login.txt");
-        $pass = file_get_contents("../keys/pass.txt");        
-        $connectionInfo = array("UID" => $login, "pwd" => $pass, "Database" => "kalendarz", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-        $serverName = "tcp:kalendarz-sqldb.database.windows.net,1433";
-        $conn = sqlsrv_connect($serverName, $connectionInfo);
-    }
-    catch (PDOException $e) {
-        print("Error connecting to SQL Server.");
-        die(print_r($e));
-    }
+        try {
+            $connectionInfo = array("UID" => $this->login, "pwd" => $this->pass, "Database" => "kalendarz", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+            $serverName = "tcp:kalendarz-sqldb.database.windows.net,1433";
+            $this->conn = sqlsrv_connect($serverName, $connectionInfo);
+            echo "\n $connectionInfo \n $servername \n";
+        }
+        catch (PDOException $e) {
+            print("Error connecting to SQL Server.");
+            die(print_r($e));
+        }
     }
     public function getConn(){
         return $this->conn;
