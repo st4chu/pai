291365ce5cdcc5c65595443a091106fc7290f8cf
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-11 20:56:09 +0200

    changed connection function

--- api/connection.php
+++ api/connection.php
@@ -1,22 +1,21 @@
 <?php
 
 class DB{
-
-    public function getConn(){
-        $this->conn = null;
-
-
+    $private conn;
+    public function __construct(){
     try {
-    $login = file_get_contents("../keys/login.txt");
-    $pass = file_get_contents("../keys/pass.txt");        
-    $this->conn = new PDO("sqlsrv:server = tcp:kalendarz-sqldb.database.windows.net,1433; Database = kalendarz", $login, $pass);
-    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $login = file_get_contents("../keys/login.txt");
+        $pass = file_get_contents("../keys/pass.txt");        
+        $this->conn = new PDO("sqlsrv:server = tcp:kalendarz-sqldb.database.windows.net,1433; Database = kalendarz", $login, $pass);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $e) {
         print("Error connecting to SQL Server.");
         die(print_r($e));
     }
-
+    }
+    public function getConn(){
+        return $this->conn;
     }
 }
 ?>
