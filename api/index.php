59f6448dc43fc6fbc86867773db76c09dfdf56d9
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-11 20:29:49 +0200

    idk

--- api/index.php
+++ api/index.php
@@ -5,10 +5,11 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+/*
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
     http_response_code(200);
     exit();
-}
+}*/
 
 include_once 'connection.php';
 include_once 'item.php';
