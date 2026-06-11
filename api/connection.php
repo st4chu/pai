c7bc8b6fbbb528a770c5019cbc65ab2bd3499ef9
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-11 20:05:29 +0200

    klamerki
    obv

--- api/connection.php
+++ api/connection.php
@@ -1,25 +1,19 @@
 <?php
 
 class DB{
-    $serverName - "tc-ms-us.database.windows.net"   ;
-    $connectionOptions = array(
-        "Database" => "kalendarz",
-        "Uid" => "kalendarz",
-        "PWD" => "QWEasd123"
-    );
 
     public function getConn(){
         $this->conn = null;
 
-   try {
-    $conn = new PDO("sqlsrv:server = tcp:tc-ms-us.database.windows.net,1433; Database = kalendarz", "kalendarz", "QWEasd123");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try {
+    $this->conn = new PDO("sqlsrv:server = tcp:kalendarz-sqldb.database.windows.net,1433; Database = kalendarz", "handler", "QWEasd123");
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $e) {
         print("Error connecting to SQL Server.");
         die(print_r($e));
     }
 
-        return $this->conn;
     }
-}
+?>
