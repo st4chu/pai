63b34592816629a7b6611a2c1ec4134d789e9bd0
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-13 16:53:55 +0200

    changed connection class

--- api/connection.php
+++ api/connection.php
@@ -1,19 +1,17 @@
 <?php
 
 class DB{
-    public $conn;
-
     public function getConn(){
-        $this->conn = null;
+        $conn = null;
 
        try{
         $login = file_get_contents('../keys/login.txt');
         $pass = file_get_contents('../keys/pass.txt')
-			$this->conn = new PDO("sqlsrv:server = tcp:kalendarz-sqldb.database.windows.net,1433; Database = kalendarz", $login, $pass);
-			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$conn = new PDO("sqlsrv:server = tcp:kalendarz-sqldb.database.windows.net,1433; Database = kalendarz", $login, $pass);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}catch(PDOException $e){
 			echo "connection error: " . $e->getMessage();
 		}
-        return $this->conn;
+        return $conn;
     }
 }
