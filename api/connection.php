6a68d0626e626ee87241f22cdbe65dd912c9cb6b
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-13 23:08:50 +0200

    fuck u azure

--- api/connection.php
+++ api/connection.php
@@ -5,9 +5,11 @@ class DB{
 
     public function getConn(){
         $this->conn = null;
-        $username = file_get_contents('login.txt');
-        $password = file_get_contents('pass.txt');
+        #$username = file_get_contents("login.txt");
+        #$password = file_get_contents("pass.txt");
 
+        $username = "handler";
+        $password ="QWEasd123";
         $connectionInfo = array("UID" => $username, "pwd" => $password, "Database" => "kalendarz", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
         $serverName = "tcp:kalendarz-sqldb.database.windows.net,1433";
         $this->conn = sqlsrv_connect($serverName, $connectionInfo);
