7fc6232128931a54e5480d44e2eaf752e4fc69a2
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-13 20:26:37 +0200

    moved txt files

--- api/connection.php
+++ api/connection.php
@@ -5,8 +5,8 @@ class DB{
 
     public function getConn(){
         $this->conn = null;
-        $username = file_get_contents('../keys/login.txt');
-        $password = file_get_contents('../keys/pass.txt');
+        $username = file_get_contents('login.txt');
+        $password = file_get_contents('pass.txt');
 
         $connectionInfo = array("UID" => $username, "pwd" => $password, "Database" => "kalendarz", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
         $serverName = "tcp:kalendarz-sqldb.database.windows.net,1433";
