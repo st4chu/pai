e7d5386f5cf3d08f123bcb72674dcf8728787d34
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-13 16:46:09 +0200

    removed junk changed connection

--- api/connection.php
+++ api/connection.php
@@ -1,17 +1,15 @@
 <?php
 
 class DB{
-    public $host = "localhost";
-    public $database = "rest";
-    public $username = "root";
-    public $password = "";
     public $conn;
 
     public function getConn(){
         $this->conn = null;
 
        try{
-			$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
+        $login = file_get_contents('../keys/login.txt');
+        $pass = file_get_contents('../keys/pass.txt')
+			$this->conn = new PDO("sqlsrv:server = tcp:kalendarz-sqldb.database.windows.net,1433; Database = kalendarz", $login, $pass);
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}catch(PDOException $e){
 			echo "connection error: " . $e->getMessage();
