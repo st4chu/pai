fa50e62873598c17d4582de730a91cabb8b3d9cf
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-13 17:13:14 +0200

    ;

--- api/connection.php
+++ api/connection.php
@@ -6,7 +6,7 @@ class DB{
     $this->conn = null;
        try{
             $login = file_get_contents('../keys/login.txt');
-            $pass = file_get_contents('../keys/pass.txt')
+            $pass = file_get_contents('../keys/pass.txt');
 			$this->conn = new PDO("sqlsrv:server = tcp:kalendarz-sqldb.database.windows.net,1433; Database = kalendarz", $login, $pass);
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}catch(PDOException $e){
