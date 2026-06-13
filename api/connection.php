2ed2dd64f84368f4d8409feecdcc0a7f40bd942a
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-13 19:51:23 +0200

    conn

--- api/connection.php
+++ api/connection.php
@@ -1,14 +1,14 @@
 <?php
 
 class DB{
-    public $username = file_get_contents('../keys/login.txt');
-    public $password = file_get_contents('../keys/pass.txt');
     public $conn;
 
     public function getConn(){
         $this->conn = null;
 
         try{
+            $username = file_get_contents('../keys/login.txt');
+            $password = file_get_contents('../keys/pass.txt');
             $this->conn = new PDO("sqlsrv:server = tcp:kalendarz-sqldb.database.windows.net,1433; Database = kalendarz", $username, $password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
