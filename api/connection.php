9719b0b96f5eee205c5079885b75868783470903
Maciej Bartłomiej Stachańczyk <mckxroyal@gmail.com>
2026-06-14 00:35:46 +0200

    minor changes

--- api/connection.php
+++ api/connection.php
@@ -1,15 +1,15 @@
 <?php
+$username = file_get_contents("login.txt");
+$password = file_get_contents("pass.txt");
 
 class DB{
     public $conn;
 
     public function getConn(){
         $this->conn = null;
-        #$username = file_get_contents("login.txt");
-        #$password = file_get_contents("pass.txt");
-
-        $username = "handler";
-        $password ="QWEasd123";
+        
+        #$username = "handler";
+        #$password ="QWEasd123";
         $connectionInfo = array("UID" => $username, "pwd" => $password, "Database" => "kalendarz", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
         $serverName = "tcp:kalendarz-sqldb.database.windows.net,1433";
         $this->conn = sqlsrv_connect($serverName, $connectionInfo);
